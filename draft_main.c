0c63a290bbaa04deb5b1373309f2a503fc15ed31
Luke Herbst <lukeherbst37@gmail.com>
2023-11-28 13:11:45 +0200

    replaced draft_main.c with contents from main.c - main.c will contain the final draft, while draft_main.c is a semi-complete version

--- draft_main.c
+++ draft_main.c
@@ -1,25 +1,49 @@
 #include "main.h"
 
-/**
-        * flip_bits - unsigned int func
-        * Description: returns the number of bits to get from n to m
-        * @n: number 1 (donor)
-        * @m: number 2 (destination)
-        * Return: number of bits flipped
-    	*/
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+int main(void)
 {
-	unsigned int bit1 = 0;
-	unsigned int bit2 = 0;
-	unsigned long int flips = 0;
-	int i;
+	char **args;
+	char *buffer;
+	char *buffer2;
+	char *token;
+	size_t buffsize = 0;
+	size_t pid;
+	int gen;
 
-	for (i = 0; i <= 31; i++)
+	buffer = malloc(100);
+	while (1)
 	{
-		bit1 = n & (1 << i);
-		bit2 = n & (1 << i);
-		if (bit1 != bit2)
-			flips++;
+		printf("$ ");
+		gen = getline(&buffer, &buffsize, stdin);
+		if (gen == -1)
+		{
+			printf("Getline failed\n");
+			return (-1);
+		}
+		printf("After getline\n");
+		buffer2 = malloc(gen);
+		strcpy(buffer2, buffer);
+		token = strtok(buffer, "\n ");
+		for (gen = 0; token != NULL; gen++)
+		{
+			token = strtok(NULL, "\n ");
+		}
+		args = malloc(gen * sizeof(char *) + 1);
+		token = strtok(buffer2, "\n ");
+		for (gen = 0; token != NULL; gen++)
+		{
+			args[gen] = malloc(strlen(token) + 1);
+			strcpy(args[gen], token);
+			token = strtok(NULL, "\n ");
+		}
+		args[gen] = NULL;
+		pid = fork();
+		if (pid == 0)
+		{
+			execve(args[0], args, NULL);
+		}
+		wait(NULL);
+		free(buffer);
+		free(buffer2);
 	}
-	return flips;
 }
