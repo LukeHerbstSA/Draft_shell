7019f82ae41473870b03eace75333d349b4dd1eb
Luke Herbst <lukeherbst37@gmail.com>
2023-12-02 11:10:27 +0200

    more changes

--- prompter.c
+++ prompter.c
@@ -11,27 +11,23 @@ char **prompt(void)
 	int gen;
 	int len;
 
+	gen = 0;
 	buffer = malloc(100);
 	line_buff = malloc(100);
 	printf("$ ");
 	while (1)
 	{
-		gen = getline(&line_buff, &buffsize, stdin);
-		if (gen == -1)
-		{
-			printf("Getline failed\n");
-			return;
-		}
-
+		gen += getline(&line_buff, &buffsize, stdin);
+		strcat(buffer, linebuff);
+		if (linebuff[-1] != '\n')
+			break;
 	}
 	printf("After getline\n");
 	buffer2 = malloc(gen);
 	strcpy(buffer2, buffer);
 	token = strtok(buffer, "\n ");
 	for (gen = 0; token != NULL; gen++)
-	{
 		token = strtok(NULL, "\n ");
-	}
 	args = malloc(gen * sizeof(char *) + 1);
 	token = strtok(buffer2, "\n ");
 	for (gen = 0; token != NULL; gen++)
